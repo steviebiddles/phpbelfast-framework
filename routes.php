5ac8ff8fe765ed57648990fbb7b3a25b7d4e04be
andy <andy@31interactive.com>
2014-08-25 09:18:06 +0100

    - added home route - changed posts paths to posts.index syntax

--- routes.php
+++ routes.php
@@ -1,10 +1,16 @@
 <?php
+$app->get('/', '\PhpBelfast\Controllers\BaseController:home')
+    ->name('home');
 
 $app->get('/hello/:name', '\PhpBelfast\Controllers\BaseController:hello');
 
 $app->group('/posts', function() use ($app){
-    $app->get('/','\PhpBelfast\Controllers\PostController:index');
+
+    $app->get('/','\PhpBelfast\Controllers\PostController:index')
+        ->name('posts.index');
+
     $app->get('/:id','\PhpBelfast\Controllers\PostController:item')
         ->conditions(['id' => '[0-9]+'])
-        ->name('post-item');
+        ->name('posts.item');
+
 });
