f012636288fd581d6d2590dd3cb5eaebd43761c1
andy <andy@31interactive.com>
2014-08-25 09:17:31 +0100

    - updated to Eloquent

--- seeder.php
+++ seeder.php
@@ -1,24 +1,38 @@
 <?php
 require "setup.php";
 
-use RedBean_Facade as R;
+use Illuminate\Database\Capsule\Manager as DB;
+
+$schema = DB::schema();
+
+$schema->dropIfExists('posts');
+
+$schema->create('posts', function(\Illuminate\Database\Schema\Blueprint $table){
+    $table->bigIncrements('id');
+    $table->string('title');
+    $table->text('summary');
+    $table->string('image');
+    $table->timestamps(); // created_at & updated_at
+});
+
 
 $faker = Faker\Factory::create();
-
-R::nuke();
-
 foreach (range(1, 20) as $idx) {
-    $post = R::dispense('post');
+    $post = new \PhpBelfast\Models\Post();
     $post->title = $faker->name() . ' ' . $faker->catchphrase();
-    $post->date = $faker->dateTimeThisMonth();
+    $post->created_at = $faker->dateTimeThisMonth();
     $post->summary = $faker->paragraph();
 
     $cats = ['abstract', 'animals', 'business', 'cats', 'city', 'food',
         'nightlife', 'fashion', 'people', 'nature', 'sports', 'technics', 'transport'];
     $category = $faker->randomElement($cats);
-    $post->image = $faker->imageUrl(300, 200, $category);
+    $post->image = $faker->imageUrl(300, 200, $category)
+                    . $faker->randomDigitNotNull();
 
-    R::store($post);
+    $post->save();
 }
 
+/*R::nuke();
 
+
+*/
