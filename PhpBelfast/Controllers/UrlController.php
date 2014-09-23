b4e7b3d5f1df7c072a93960678a11338942f1082
Stephen McAuley <steviebiddles@gmail.com>
2014-09-23 20:16:07 +0100

    full run over

---
+++ PhpBelfast/Controllers/UrlController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stephen
+ * Date: 23/09/2014
+ * Time: 19:25
+ */
+
+namespace PhpBelfast\Controllers;
+
+
+class UrlController {
+
+} 
---
+++ PhpBelfast/Models/Url.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stephen
+ * Date: 23/09/2014
+ * Time: 19:34
+ */
+
+namespace PhpBelfast\Models;
+
+
+class Url {
+
+} 
