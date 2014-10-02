a287f7e6d2b1cd75513e79e040a7588695eb887e
Stephen McAuley <steviebiddles@gmail.com>
2014-10-02 21:02:58 +0100

    live code

---
+++ PhpBelfast/Models/Url.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stephen
+ * Date: 02/10/2014
+ * Time: 19:28
+ */
+
+namespace PhpBelfast\Models;
+
+
+class Url {
+
+} 
---
+++ PhpBelfast/Utilities/Convert.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stephen
+ * Date: 02/10/2014
+ * Time: 19:36
+ */
+
+namespace PhpBelfast\Utilities;
+
+
+class Convert {
+
+} 
